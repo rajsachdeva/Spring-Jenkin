3bc3c63d21aa2316eb80642d8e1064949a91a471
rajeevsachdeva <rsachdeva516@gmail.com>
2021-04-15 17:12:15 +0800

    second commit with check error

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public contextLoads() {
 		System.out.println("Test cases executing");
 		System.out.println("Test cases executing again for confirm the build");
 		assertEquals(true, true);
